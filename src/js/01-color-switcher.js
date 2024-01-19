f6d289af3d9034e0738b1df8fc9fed15543bde7c
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-01-19 20:17:09 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const elements = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let intervalId = null;
+
+function startClickHandler() {
+    elements.startBtn.disabled = true;
+    intervalId = setInterval(() => {
+        elements.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopClickHandler() {
+    elements.startBtn.disabled = false;
+    clearInterval(intervalId);
+}
+
+elements.startBtn.addEventListener('click', startClickHandler);
+
+elements.stopBtn.addEventListener('click', stopClickHandler);
